e4295d03eb834c947a764227fdbe234661771b39
srihita-e <77421310+srihita-e@users.noreply.github.com>
2021-03-29 21:42:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -15,6 +20,10 @@ function modelLoaded() {
 
 function draw() {
     background('#CBC3E3')
+    document.getElementById('square_side').innerHTML = "widht and height of a square will be" + difference + "px";
+    fill('#add8e6');
+    stroke('#add8e6');
+    square(noseX, noseY, difference);
 }
 
 function gotPoses(results)
@@ -22,5 +31,12 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWrist = " + leftWristX + "rightWristX" + rightWristX + "difference = " + difference);
     }
 }
